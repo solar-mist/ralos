24b6d2bc95eddd513557e54ed03fe74876b388c0
solar-mist <solarmistreal@gmail.com>
2023-03-25 10:02:26 +0000

    Add GDT

--- kernel/src/kernel/kernel.cpp
+++ kernel/src/kernel/kernel.cpp
@@ -1,12 +1,14 @@
+#include <cpu/gdt/gdt.hpp>
 #include <drivers/graphics.hpp>
 #include <drivers/terminal.hpp>
 
 extern "C" void KMain()
 {
+    InitGDT();
     Graphics::Init();
     Terminal::Init();
-    Terminal::Clear(0xFF0000);
+    Terminal::Clear(0x018281);
     for(int i = 0; i < 256; i++)
-        Terminal::Print("Hello world\n");
+        Terminal::Print("Hello world\n", 0x606060);
     for(;;);
 }  
